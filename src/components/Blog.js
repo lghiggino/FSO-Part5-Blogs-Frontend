1cb1ad1c091f9d393a80aaee3e064b261b5fb017
lghiggino <lghiggino@gmail.com>
2022-12-15 08:18:31 -0300

    6.6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+import Togglable from "./Toggable";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <p>
+          {blog.title} {blog.author}
+        </p>
+        <button
+          onClick={toggleVisibility}
+          style={{ textTransform: "capitalize" }}
+        >
+          View
+        </button>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p> {blog.url}</p>
+        <p> {blog.likes}</p>
+      </div>
+    </>
+  );
+};
 
 export default Blog;
