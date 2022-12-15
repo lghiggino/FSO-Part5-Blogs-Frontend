3805464a10b9cd82422e281e4db267dc94b0beda
lghiggino <lghiggino@gmail.com>
2022-12-15 08:05:42 -0300

    refactor: Togglable is more flexible

--- src/components/Toggable.js
+++ src/components/Toggable.js
@@ -1,38 +1,42 @@
 /* eslint-disable react/display-name */
 import { useState, forwardRef, useImperativeHandle } from "react";
 
-const Togglable = forwardRef(({ buttonLabel, children }, refs) => {
-  const [visible, setVisible] = useState(false);
+const Togglable = forwardRef(
+  ({ buttonLabel, closeButtonLabel = "cancel", children }, refs) => {
+    const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? "none" : "" };
-  const showWhenVisible = { display: visible ? "" : "none" };
+    const hideWhenVisible = { display: visible ? "none" : "" };
+    const showWhenVisible = { display: visible ? "" : "none" };
 
-  const toggleVisibility = () => {
-    setVisible(!visible);
-  };
-
-  useImperativeHandle(refs, () => {
-    return {
-      toggleVisibility,
+    const toggleVisibility = () => {
+      setVisible(!visible);
     };
-  });
 
-  return (
-    <div>
-      <div style={hideWhenVisible}>
-        <button
-          onClick={toggleVisibility}
-          style={{ textTransform: "capitalize" }}
-        >
-          {buttonLabel}
-        </button>
+    useImperativeHandle(refs, () => {
+      return {
+        toggleVisibility,
+      };
+    });
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button
+            onClick={toggleVisibility}
+            style={{ textTransform: "capitalize" }}
+          >
+            {buttonLabel}
+          </button>
+        </div>
+        <div style={showWhenVisible}>
+          {children}
+          <button onClick={toggleVisibility}>
+            {closeButtonLabel || "cancel"}
+          </button>
+        </div>
       </div>
-      <div style={showWhenVisible}>
-        {children}
-        <button onClick={toggleVisibility}>cancel</button>
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Togglable;
